f81f6ad7a3735e40d521538941101ce985a4fed9
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-01-12 15:16:39 +0000

    Accommodate API change to PluginBase, which now requires that subclasses define an instance method getLanguageID().

--- src/org/eclipse/imp/lpg/analysis/LPGAnalysisPlugin.java
+++ src/org/eclipse/imp/lpg/analysis/LPGAnalysisPlugin.java
@@ -17,10 +17,8 @@ import java.net.URL;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.preferences.DefaultScope;
 import org.eclipse.imp.preferences.PreferencesService;
 import org.eclipse.imp.runtime.PluginBase;
-import org.eclipse.imp.utils.ExtensionFactory;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.resource.ImageRegistry;
 import org.osgi.framework.Bundle;
@@ -32,14 +30,14 @@ import org.osgi.framework.BundleContext;
 public class LPGAnalysisPlugin extends PluginBase {
     public static final String kPluginID= "org.eclipse.imp.lpg.analysis";
 
+    private static final String kLanguageID= "lpgAnalysis";
+
     /**
      * The unique instance of this plugin class
      */
     protected static LPGAnalysisPlugin sPlugin;
 
     protected static PreferencesService preferencesService = null;
-
-    private static String kLanguageID;
 
     public static LPGAnalysisPlugin getInstance() {
         return sPlugin;
@@ -50,10 +48,16 @@ public class LPGAnalysisPlugin extends PluginBase {
         sPlugin= this;
     }
 
+    public String getLanguageID() {
+        return kLanguageID;
+    }
+
+    public String getID() {
+        return kPluginID;
+    }
+
     public void start(BundleContext context) throws Exception {
         super.start(context);
-
-        kLanguageID= ExtensionFactory.retrieveLanguageIdFromPlugin(kPluginID);
 
         // Initialize the LPGPreferences fields with the preference store data.
         if (preferencesService == null) {
@@ -77,33 +81,7 @@ public class LPGAnalysisPlugin extends PluginBase {
         return null;
     }
 
-    public static String getLanguageID() {
-        return kLanguageID;
-    }
-
-    public String getID() {
-        return kPluginID;
-    }
-    
-    public static PreferencesService getPreferencesService() {
-    	if (preferencesService == null) {
-        	preferencesService = new PreferencesService();
-        	preferencesService.setLanguageName(kLanguageID);
-        	
-    		// To trigger the automatic invocation of the preferences initializer:
-    		try {
-    			new DefaultScope().getNode(kPluginID);
-    		} catch (Exception e) {
-    			// If this ever happens, it will probably be because the preferences
-    			// and their initializer haven't been defined yet.  In that situation
-    			// there's not really anything to do--you can't initialize preferences
-    			// that don't exist.  So swallow the exception and continue ...
-    		}
-    	}
-    	return preferencesService;
-    }
-
-    // Overwriting method in IMPPluginBase because at that level we don't have
+    // Overriding method in IMPPluginBase because at that level we don't have
     // a preferences service to query dynamically, only a field set from this level
     // at the time of preference initialization
     public void maybeWriteInfoMsg(String msg) {
